b27ec3465b76190db1a36ca043518ed6067ad3f7
Navneet Pandey <13730@teamcomputers.com.com>
2024-04-05 11:46:34 +0530

    commit test

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,14 @@ import express from 'express';
 const app = express();
 
 app.get('*', (req, res) => {
-
-
-
-
-
-              res.status(200).send({
+  res.status(200).send({
     success: true,
-              message: 'route is running properly!!!!!'
-            });
+    message: 'route is running properly!!!!!'
+  });
 
 
 
-
-
-
+  
 });
 
 export default app;
