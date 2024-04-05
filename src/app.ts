5cd2449c2e6334dd8e125d3b6374b509edc43d40
Navneet Pandey <me_navneet@hotmail.com>
2024-04-05 12:13:01 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,14 @@ import express from 'express';
 const app = express();
 
 app.get('*', (req, res) => {
-
-
   res.status(200).send({
     success: true,
-
-    message: 'route is running properly it has been checked!!!!!'
+    message: 'route is running properly it has been checked this to be added again!!!!!'
+  
+  
+  
+  
   });
-
-
-
-
-
 });
 
 export default app;
